cb6ba07974426dd6f59e8e9347c9b82e9cbce271
Ariel Mannes <arielmannes@gmail.com>
2018-02-11 17:27:40 +0200

    "self merge of the working model"

--- gender_classifier.py
+++ gender_classifier.py
@@ -169,68 +169,72 @@ y = encoder.fit_transform(list(neural_data_set[i][1] for i in range(0, len(neura
 #y = y[:500]
 
 
-#max_words = 4000
-max_words = 40
+max_words = 4000
+#max_words = 40
 k_tokenizer = keras_token(num_words=max_words)
 k_tokenizer.fit_on_texts(x)
-dictionary = k_tokenizer.word_index
-
-def convert_text_to_index_array(text):
-    # one really important thing that `text_to_word_sequence` does
-    # is make all texts the same length -- in this case, the length
-    # of the longest text in the set.
-    return [dictionary[word] for word in kpt.text_to_word_sequence(text)]
-
-def x_preprocessing(x):
-    allWordIndices = []
-    # for each tweet, change each token to its ID in the Tokenizer's word_index
-    for text in x:
-        wordIndices = convert_text_to_index_array(text)
-        allWordIndices.append(wordIndices)
-
-    # now we have a list of all tweets converted to index arrays.
-    # cast as an array for future usage.
-    allWordIndices = np.asarray(allWordIndices)
-    return allWordIndices
-
-
-# create one-hot matrices out of the indexed tweets
-x = k_tokenizer.sequences_to_matrix(x_preprocessing(x), mode='binary')
+x = k_tokenizer.texts_to_sequences(x)
+x = sequence.pad_sequences(x)
 # treat the labels as categories
 y = keras.utils.to_categorical(y, 2)
 
 
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state= 42)
 
-max_text_length = 500
-embedding_vecor_length = 32
+
+
+embed_dim = 128
+lstm_out = 196
+
 model = Sequential()
-
-
-model.add(Embedding(len(top_words), embedding_vecor_length,input_length=max_text_length))
-model.add(LSTM(100))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.add(Embedding(max_words, embed_dim,input_length = x.shape[1]))
+model.add(LSTM(lstm_out))
+model.add(Dense(2,activation='softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
 print(model.summary())
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=128)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=7, batch_size=256)
 
 # Final evaluation of the model
+validation_size = 500
+X_validate = X_test[-validation_size:]
+Y_validate = y_test[-validation_size:]
+X_test = X_test[:-validation_size]
+y_test = y_test[:-validation_size]
+
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
 
+pos_cnt, neg_cnt, pos_correct, neg_correct = 0, 0, 0, 0
+for x in range(len(X_validate)):
+
+    result = model.predict(X_validate[x].reshape(1, X_test.shape[1]), batch_size=1, verbose=2)[0]
+
+    if np.argmax(result) == np.argmax(Y_validate[x]):
+        if np.argmax(Y_validate[x]) == 0:
+            neg_correct += 1
+        else:
+            pos_correct += 1
+
+    if np.argmax(Y_validate[x]) == 0:
+        neg_cnt += 1
+    else:
+        pos_cnt += 1
+
+print("pos_acc", pos_correct / pos_cnt * 100, "%")
+print("neg_acc", neg_correct / neg_cnt * 100, "%")
 # q 4  - testint the model on data that was collected in real time
 
 
-raw_tweets = pd.read_csv('realTime.csv', encoding='utf8')
-list_tweets = raw_tweets.values.tolist()
-
+# raw_tweets = pd.read_csv('realTime.csv', encoding='utf8')
+# list_tweets = raw_tweets.values.tolist()
 #
-to_predict = k_tokenizer.sequences_to_matrix(x_preprocessing(list_tweets), mode='binary')
-
-predictions = model.predict(to_predict, batch_size = 200)
-
-print(predictions)
+# #
+# to_predict = k_tokenizer.sequences_to_matrix(x_preprocessing(list_tweets), mode='binary')
+#
+# predictions = model.predict(to_predict, batch_size = 200)
+#
+# print(predictions)
 
 
 
